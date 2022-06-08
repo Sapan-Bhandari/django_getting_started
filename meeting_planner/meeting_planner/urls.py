f4e5c3b77e9eda0582e1b473ed069fe44756e0eb
Sapan Bhandari <sapanbhandari01@gmail.com>
2022-06-08 17:20:03 -0400

    Second Project Commit

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from website.views import welcome, date, about
-
+from website_mine.views import welcome, date, about
+from meetings.views import detail, rooms_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name="welcome"),
     path('date', date),
     path('about', about),
-    path('meetings/', include('meetings.urls')),
+    path('meetings/', include('meetings.urls'))
 ]
